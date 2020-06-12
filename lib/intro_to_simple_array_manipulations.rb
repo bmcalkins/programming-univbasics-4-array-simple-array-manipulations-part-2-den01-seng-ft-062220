2cd3e4e4aefefa25937c14c2efee425c0edc8a13
Bradley Calkins <b.raders1013@gmail.com>
2020-06-12 20:21:34 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_concat(my_favorite_things, more_favs)
+my_favorite_things.concat more_favs
+end
+
+def using_insert(array, element)
+  array.insert(4, "Python")
+end
+
+def using_uniq(haircuts)
+  haircuts.uniq()
+  
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  
